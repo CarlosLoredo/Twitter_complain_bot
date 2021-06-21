88af5ddc7d40a5af8570b5152053350057267e8d
CarlosLoredo <carlos.loredo.arancibia@gmail.com>
2021-06-21 16:24:23 -0400

    Add a comment

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ chrome_driver_path = '/home/lobo/PycharmProjects/chromedriver_linux64/chromedriv
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 web_content = driver.get("https://twitter.com/login")
 print(web_content)
+#Add a comment
