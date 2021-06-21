33b1dfff10dc69b77d54e988b48e3153e066a0c0
CarlosLoredo <carlos.loredo.arancibia@gmail.com>
2021-06-21 16:21:59 -0400

    Add a print statement

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 
 chrome_driver_path = '/home/lobo/PycharmProjects/chromedriver_linux64/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://twitter.com/login")
-
+web_content = driver.get("https://twitter.com/login")
+print(web_content)
